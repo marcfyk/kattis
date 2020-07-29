49fc201e0da562e40eb1e93aa082e8208beb516e
marcfyk <marcjohnfong@gmail.com>
2020-07-29 10:34:43 +0800

    Add solution

--- src/TaisFormula/Main.java
+++ src/TaisFormula/Main.java
@@ -4,11 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        int[] T = new int[N];
+        double[] V = new double[N];
         for (int i = 0; i < N; ++i) {
-            int t = sc.nextInt();
-            double v = sc.nextDouble();
-
+            T[i] = sc.nextInt();
+            V[i] = sc.nextDouble();
         }
+
+        double area = 0;
+        int index = 0;
+        int prevT = T[index];
+        double prevV = V[index];
+        while (index < N - 1) {
+            int nextT = T[index + 1];
+            double nextV = V[index + 1];
+            area += (((prevV + nextV) / 2) * (nextT - prevT)) / 1000;
+            prevT = nextT;
+            prevV = nextV;
+            ++index;
+        }
+
+        System.out.println(area);
         sc.close();
     }
 }
